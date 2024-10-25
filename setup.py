ecf1c3205fe08ab81fbc08ad58e93c343c0dcf2f
Hendra Manudinata <hendra@manoedinata.com>
2024-10-25 13:18:44 +0700

    v0.3: `natports`: [NAT] Setup 20 port forwardings for basic use, automatically

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="VirtuCLI",
-    version="0.2",
+    version="0.3",
     author="manoedinata",
     author_email="manoedinata@gmail.com",
     description="Basic management of Virtualizor VMs from CLI.",
--- virtucli/main.py
+++ virtucli/main.py
@@ -2,6 +2,7 @@ from argparse import ArgumentParser
 from configparser import ConfigParser
 from appdirs import user_config_dir
 
+import random
 from virtualizorapi import Api
 
 from .utils import listVM
@@ -18,13 +19,19 @@ def init_args():
 
     # Subcommand
     subparsers = parser.add_subparsers(dest="command", required=True)
-    
+
     ## List VM
     listVM = subparsers.add_parser("listvm", help="List available VMs")
 
     ## VM info
     VMInfo = subparsers.add_parser("vminfo", help="Get specific VM info")
     VMInfo.add_argument("-i", "--id", help="VM UID", required=True)
+
+    ## Domain Forwarding: Setup 20 ports
+    natPorts = subparsers.add_parser("natports", help="[NAT] Setup 20 port forwardings for basic use, automatically")
+    natPorts.add_argument("-i", "--id", help="VM UID", required=True)
+    natPorts.add_argument("-p", "--ports", help="Base ports to be used. For example, if 27000 is specified, then the added ports will be 27000, 27001, 27002, until 27020. Random ports will be used if not specified.", type=int, required=False)
+    natPorts.add_argument("--ssh", help="Use the first port for SSH port.", action="store_true", required=False)
 
     # Parse arguments
     args = parser.parse_args()
@@ -50,3 +57,23 @@ def main():
 
     elif args.command == "vminfo":
         getVMInfo(api, args.id)
+
+    elif args.command == "natports":
+        ports = args.ports
+        if not ports: ports = random.randint(25001, 64000)
+        lengthOfPorts = 20
+
+        # Determine which IP to be used (random, shall we?)
+        vdfInfo = api.getVDFInfo(args.id)
+        src_ips = random.choice(vdfInfo["src_ips"])
+        dest_ips = random.choice(vdfInfo["dest_ips"])
+
+        # SSH
+        if args.ssh:
+            api.addVDF(args.id, "TCP", port, src_ips, dest_ips, 22)
+            ports += 1
+            lengthOfPorts -= 1
+
+        # Add ports
+        for port in range(ports, ports + lengthOfPorts):
+            api.addVDF(args.id, "TCP", port, src_ips, dest_ips, port)
